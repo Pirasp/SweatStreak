1904e11702cad2d0c576360c19ff706e1636bbbb
Pirasp <johanneshofer@gmx.net>
2023-04-12 14:28:51 +0200

    Created Ionic Project
    the start of the Ionic project

---
+++ SweatStreak/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SweatStreak',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
